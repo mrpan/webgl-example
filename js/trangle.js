061b29b886a5a43b1124ca3f72f50b9e814a3f90
mrpan <543421341@qq.com>
2020-03-15 12:33:30 +0800

    add first example

---
+++ js/trangle.js
@@ -0,0 +1,34 @@
+
+
+var VSHADER_SOURCE = 
+  'void main() {\n' +
+  '  gl_Position = vec4(0.0, 0.0, 0.0, 1.0);\n' + // Set the vertex coordinates of the point
+  '  gl_PointSize = 10.0;\n' +                    // Set the point size
+  '}\n';
+
+// Fragment shader program
+var FSHADER_SOURCE =
+  'void main() {\n' +
+  '  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' + // Set the point color
+  '}\n';
+
+function init(){
+    var canvas =document.getElementById("canvas");
+    var gl =createGLContext(canvas);
+    // Initialize shaders
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to intialize shaders.');
+        return;
+    }
+
+  // Specify the color for clearing <canvas>
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+  // Clear <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  // Draw a point
+  gl.drawArrays(gl.POINTS, 0, 1);
+}
+
+init();
